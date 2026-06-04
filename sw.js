f0579e21ef91977770dd28e937dc1065a14b9a7f
a717418756-ux <a717418756@gmail.com>
2026-06-04 08:52:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    ★ 每次更新程式只需修改 APP_VERSION
    ══════════════════════════════════════════════════════════════ */
 
-const APP_VERSION = '1.5.3';
+const APP_VERSION = '1.5.4';
 const CACHE_NAME  = `yc-cache-${APP_VERSION}`;
 
 // ── 快取資源（不包含 index.html）────────────────────────────
